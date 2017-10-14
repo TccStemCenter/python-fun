5c4491607be5d4b85b0671fc018d5edcd2f93785
tccstem <tccstemcenter@protonmail.ch>
2017-10-14 18:19:59 -0400

    ball class with movement and slight improvements

--- pong.py
+++ pong.py
@@ -1,19 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from tkinter import *
 
-def keylistener(event):
-    print(event)
+class Ball():
+    '''Ball for pong'''
 
+    def __init__(self, canvas, radius, vx, vy, x = 0, y = 0, color="red"):
+        '''initialize ball for a canvas with radius with velocity in
+            x- and y-direction and, optionally, at (x,y) position
+
+        :canvas: canvas to which to draw the ball
+        :radius: radius of the ball
+        :vx: velocity in x-direction
+        :vy: velocity in y-direction
+        :x: initial x-position (0 if not specified)
+        :y: initial y-position (0 if not specified)
+
+        '''
+        self._radius = radius
+        self._canvas = canvas
+        self._vx = vx
+        self._vy = vy
+        self._oval = self._canvas.create_oval(x - radius, y - radius, x + radius, y + radius, fill=color, outline="")
+
+    def updatePosition(self):
+        coords = self._canvas.coords(self._oval)
+        if self._vx > 0:
+            if coords[2] + self._vx > self._canvas.winfo_reqwidth():
+                self._vx *= -1
+        else:
+            if coords[0] + self._vx < 0:
+                self._vx *= -1
+        if self._vy > 0:
+            if coords[3] + self._vy > self._canvas.winfo_reqheight():
+                self._vy *= -1
+        else:
+            if coords[1] + self._vy < 0:
+                self._vy *= -1
+        self._canvas.move(self._oval, self._vx, self._vy)
+
+def keylistener(event):
+    '''handles all key input
+
+    :event: event passed by Tk mainloop
+    '''
+    if event.char == 'q':
+        # exit out of application
+        root.destroy()
+        return
+    if event.char == 'w':
+        pass
+    if event.char == 's':
+        pass
+    if event.char == 'o':
+        pass
+    if event.char == 'l':
+        pass
+
+# create window
 root = Tk()
+
+# create canvas (for drawing) and place into root
 canvas = Canvas(root, width=400, height=400)
-canvas.create_oval(50, 50, 60, 60, fill="red")
+
+# create ball
+ball = Ball(canvas, 8, 3, 1, 200, 200, "blue")
+
+# bind to keys
+for key in ("w", "s", "o", "l", "q"):
+    root.bind(key, keylistener)
+
+# update root elements
 canvas.pack()
-root.bind("w", keylistener)
-root.bind("s", keylistener)
-root.bind("o", keylistener)
-root.bind("l", keylistener)
 
 def animate():
-    # TODO: make useful code
+    '''animation loop (async)
+    '''
+    ball.updatePosition()
     root.after(20, animate)
 
 animate()
